b425432a2c4597897c3b0fa700c780ea713a7cf3
Roger Terrazas <rogert9801@gmail.com>
2025-06-29 16:09:07 -0500

    fix build

---
+++ apps/backend/.next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+const path = require("path");
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
